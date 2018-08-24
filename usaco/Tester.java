01ffb313928257ca61bb06e52f5c4afb3ef709e1
gamester543 <gamesterrex@gmail.com>
2018-08-24 14:38:23 -0700

    Updated Tester file

--- usaco/Tester.java
+++ usaco/Tester.java
@@ -1,8 +1,18 @@
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
@@ -12,223 +22,245 @@ import java.util.zip.ZipFile;
  * Created by Robert on 8/5/2018.
  */
 public class Tester {
-    public static final String PROBLEM = "mincross";
-    public static final String DATA_PATH = "./data";
-    public static final File DATA_DIR = new File(DATA_PATH);
-    public static final File UNZIP_DIR = new File(DATA_PATH + "/tmp");
-    public static final int MAX_RUNTIME = 3 * 1000;
+	public static final String PROBLEM = "cardgame";
 
-    static final File INPUT_FILE = new File(PROBLEM + ".in");
-    static final File OUTPUT_FILE = new File(PROBLEM + ".out");
+	public static final String TEMPLATE = "import java.io.File;\r\nimport java.io.FileInputStream;\r\nimport java.io.FileOutputStream;\r\nimport java.io.PrintStream;\r\nimport java.util.Scanner;\r\n\r\npublic class %1$s {\r\n\r\n\tpublic static void main(String args[]) throws Exception {\r\n\t\tboolean testing = new File(\"%1$s.in\").exists();\r\n\t\tScanner in = new Scanner(testing ? new FileInputStream(\"%1$s.in\") : System.in);\r\n\t\tPrintStream out = new PrintStream(testing ? new FileOutputStream(\"%1$s.out\") : System.out);\r\n\r\n\t\tin.close();\r\n\t\tout.close();\r\n\t}\r\n}\r\n";
+	public static final String CURRENT_TEMPLATE = String.format(TEMPLATE, PROBLEM);
+	public static final String DATA_PATH = "./data";
+	public static final File DATA_DIR = new File(DATA_PATH);
+	public static final File UNZIP_DIR = new File(DATA_PATH + "/tmp");
+	public static final int MAX_RUNTIME = 3 * 1000;
 
-    public static final Logger logger = Logger.getGlobal();
+	static final File INPUT_FILE = new File(PROBLEM + ".in");
+	static final File OUTPUT_FILE = new File(PROBLEM + ".out");
 
-    public static void main(String[] args) {
-        logger.setLevel(Level.INFO);
+	public static final Logger logger = Logger.getGlobal();
 
-        extractFiles(UNZIP_DIR);
+	public static void main(String[] args) {
+		logger.setLevel(Level.INFO);
 
-        TestResult[] results = runTests(UNZIP_DIR);
-        printResults(results);
+		if (generateProblemFile()) {
+			extractFiles(UNZIP_DIR);
 
-        cleanup(DATA_DIR);
-    }
+			TestResult[] results = runTests(UNZIP_DIR);
+			printResults(results);
 
-    public static void extractFiles(File unzipDir) {
-        try {
-            extractFilesHelper(unzipDir);
-        } catch (IOException ie) {
-            logger.severe("Failed to extract files");
-            logger.severe(ie.getMessage());
-        }
-    }
+			cleanup(DATA_DIR);
+		} else {
+			logger.info(String.format("Generated problem file in src/%s.java", PROBLEM));
+		}
 
-    public static TestResult[] runTests(File unzipDir) {
-        try {
-            return runTestsHelper(unzipDir);
-        } catch (IOException | ReflectiveOperationException e) {
-            logger.severe("Failed to run tests");
-            logger.severe(e.getMessage());
-        }
-        return null;
-    }
+	}
 
-    public static void printResults(TestResult[] results) {
-        if (results == null) return;
+	public static boolean generateProblemFile() {
+		File problemFile = new File(String.format("src/%s.java", PROBLEM));
+		if (problemFile.exists()) {
+			return true;
+		}
+		try {
+			problemFile.createNewFile();
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("\n");
-        for (TestResult result : results) {
-            sb.append(result + "\n");
-        }
+			try {
+				PrintStream out = new PrintStream(problemFile);
+				out.println(CURRENT_TEMPLATE);
+				out.close();
+			} catch (IOException e) {
+				logger.severe("Failed to write template to file.");
+				logger.severe(e.getMessage());
+			}
+		} catch (IOException e) {
+			logger.severe("Failed to create new problem file.");
+			logger.severe(e.getMessage());
+		}
+		return false;
+	}
 
-        logger.info(sb.toString());
-    }
+	public static void extractFiles(File unzipDir) {
+		try {
+			extractFilesHelper(unzipDir);
+		} catch (IOException ie) {
+			logger.severe("Failed to extract files");
+			logger.severe(ie.getMessage());
+		}
+	}
 
-    static class TestResult {
-        boolean success;
+	public static TestResult[] runTests(File unzipDir) {
+		try {
+			return runTestsHelper(unzipDir);
+		} catch (IOException | ReflectiveOperationException e) {
+			logger.severe("Failed to run tests");
+			logger.severe(e.getMessage());
+		}
+		return null;
+	}
 
-        boolean match;
-        boolean error;
-        double runTime;
+	public static void printResults(TestResult[] results) {
+		if (results == null) return;
 
-        int testCase;
+		StringBuilder sb = new StringBuilder();
+		sb.append("\n");
+		for (TestResult result : results) {
+			sb.append(result + "\n");
+		}
 
-        TestResult(int testCase, boolean match, boolean error, double runTime) {
-            this.testCase = testCase;
-            this.match = match;
-            this.error = error;
-            this.runTime = runTime / 1000;
+		logger.info(sb.toString());
+	}
 
-            success = (match && !error && runTime < MAX_RUNTIME);
-        }
+	static class TestResult {
+		boolean success;
 
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            sb.append(String.format("#%2d | ", testCase));
-            if (success) {
-                sb.append(String.format("* %.2f", runTime));
-            } else if (error) {
-                sb.append("!");
-            } else if (!match) {
-                sb.append("WA");
-            } else {
-                sb.append(String.format("TLE %.2f", runTime));
-            }
-            return sb.toString();
-        }
-    }
+		boolean match;
+		boolean error;
+		double runTime;
 
-    public static TestResult[] runTestsHelper(File unzipDir) throws IOException, ReflectiveOperationException {
-        List<TestResult> results = new ArrayList<>();
+		int testCase;
 
-        int fileCount = unzipDir.listFiles().length;
-        String unzipPath = unzipDir.getCanonicalPath();
+		TestResult(int testCase, boolean match, boolean error, double runTime) {
+			this.testCase = testCase;
+			this.match = match;
+			this.error = error;
+			this.runTime = runTime / 1000;
 
-        logger.finer("Running tests...");
-        for (int testCase = 1; testCase <= fileCount / 2; testCase++) {
-            try {
-                setupTestData(testCase, unzipPath);
-            } catch (IllegalArgumentException iae) {
-                logger.severe(iae.getMessage());
-                continue;
-            }
+			success = (match && !error && runTime < MAX_RUNTIME);
+		}
 
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append(String.format("#%2d | ", testCase));
+			if (success) {
+				sb.append(String.format("* %.2f", runTime));
+			} else if (error) {
+				sb.append("!");
+			} else if (!match) {
+				sb.append("WA");
+			} else {
+				sb.append(String.format("TLE %.2f", runTime));
+			}
+			return sb.toString();
+		}
+	}
 
-            logger.info(String.format("Running test case %d.", testCase));
-            File output = new File(String.format("%s/%d.out", unzipPath, testCase));
-            TestResult result = runTest(testCase, output);
-            results.add(result);
+	public static TestResult[] runTestsHelper(File unzipDir) throws IOException, ReflectiveOperationException {
+		List<TestResult> results = new ArrayList<>();
 
-            System.gc();
+		int fileCount = unzipDir.listFiles().length;
+		String unzipPath = unzipDir.getCanonicalPath();
 
-            logger.info(result.toString());
+		logger.finer("Running tests...");
+		for (int testCase = 1; testCase <= fileCount / 2; testCase++) {
+			try {
+				setupTestData(testCase, unzipPath);
+			} catch (IllegalArgumentException iae) {
+				logger.severe(iae.getMessage());
+				continue;
+			}
 
-            if (testCase == 1 && !result.success) {
-                break;
-            }
-        }
+			logger.info(String.format("Running test case %d.", testCase));
+			File output = new File(String.format("%s/%d.out", unzipPath, testCase));
+			TestResult result = runTest(testCase, output);
+			results.add(result);
 
-        INPUT_FILE.delete();
-        OUTPUT_FILE.delete();
+			System.gc();
 
-        Collections.sort(results, (a, b) -> a.testCase - b.testCase);
-        return results.toArray(new TestResult[results.size()]);
-    }
+			logger.info(result.toString());
 
-    public static void setupTestData(int testCase, String unzipPath) throws IOException {
-        File input = new File(String.format("%s/%d.in", unzipPath, testCase));
-        File output = new File(String.format("%s/%d.out", unzipPath, testCase));
+			if (testCase == 1 && !result.success) {
+				break;
+			}
+		}
 
-        if (!input.exists() || !output.exists()) {
-            throw new IllegalArgumentException(String.format("Invalid test data for #%d", testCase));
-        }
+		INPUT_FILE.delete();
+		OUTPUT_FILE.delete();
 
+		Collections.sort(results, (a, b) -> a.testCase - b.testCase);
+		return results.toArray(new TestResult[results.size()]);
+	}
 
-        INPUT_FILE.delete();
-        Files.copy(input.toPath(), INPUT_FILE.toPath());
-        OUTPUT_FILE.delete();
-        OUTPUT_FILE.createNewFile();
-    }
+	public static void setupTestData(int testCase, String unzipPath) throws IOException {
+		File input = new File(String.format("%s/%d.in", unzipPath, testCase));
+		File output = new File(String.format("%s/%d.out", unzipPath, testCase));
 
-    public static TestResult runTest(int testCase, File output) throws ReflectiveOperationException, IOException {
-        Class solution = Class.forName(PROBLEM);
-        Method solutionMain = solution.getMethod("main", String[].class);
+		if (!input.exists() || !output.exists()) {
+			throw new IllegalArgumentException(String.format("Invalid test data for #%d", testCase));
+		}
 
-        boolean error = false, match = false;
-        String[] args = new String[1];
+		INPUT_FILE.delete();
+		Files.copy(input.toPath(), INPUT_FILE.toPath());
+		OUTPUT_FILE.delete();
+		OUTPUT_FILE.createNewFile();
+	}
 
-        long startTime = System.currentTimeMillis(), runTime = MAX_RUNTIME;
+	public static TestResult runTest(int testCase, File output) throws ReflectiveOperationException, IOException {
+		Class solution = Class.forName(PROBLEM);
+		Method solutionMain = solution.getMethod("main", String[].class);
 
-        //If not the first test, silence stdout.
-        if(testCase != 1){
-            System.setOut(new PrintStream(new ByteArrayOutputStream()));
-        }
-        try {
-            solutionMain.invoke(null, args);
+		boolean error = false, match = false;
+		String[] args = new String[1];
 
-        } catch (InvocationTargetException ite) {
-            if (testCase == 1) {
-                ite.getCause().printStackTrace();
-            }
-            error = true;
-        }
-        if (!error) {
-            runTime = System.currentTimeMillis() - startTime;
+		long startTime = System.currentTimeMillis(), runTime = MAX_RUNTIME;
 
-            match = filesEqual(OUTPUT_FILE, output);
-        }
-        return new TestResult(testCase, match, error, runTime);
-    }
+		// If not the first test, silence stdout.
+		if (testCase != 1) {
+			System.setOut(new PrintStream(new ByteArrayOutputStream()));
+		}
+		try {
+			solutionMain.invoke(null, args);
 
-    public static boolean filesEqual(File solutionOutputFile, File testOutputFile) throws IOException {
-        try (
-            Scanner solutionOutput = new Scanner(new BufferedInputStream(new FileInputStream(solutionOutputFile)));
-            Scanner testOutput = new Scanner(new BufferedInputStream(new FileInputStream(testOutputFile)));
-        ) {
-            while (solutionOutput.hasNextLine() && testOutput.hasNextLine()) {
-                if (!solutionOutput.nextLine().equals(testOutput.nextLine())) {
-                    return false;
-                }
-            }
-            return solutionOutput.hasNextLine() == testOutput.hasNextLine();
-        }
-    }
+		} catch (InvocationTargetException ite) {
+			if (testCase == 1) {
+				ite.getCause().printStackTrace();
+			}
+			error = true;
+		}
+		if (!error) {
+			runTime = System.currentTimeMillis() - startTime;
 
-    public static void extractFilesHelper(File unzipDir) throws IOException {
-        //If the directory already existed, we may have to clean up old files
-        if (!unzipDir.mkdir()) {
-            cleanup(unzipDir);
-        }
+			match = filesEqual(OUTPUT_FILE, output);
+		}
+		return new TestResult(testCase, match, error, runTime);
+	}
 
-        logger.log(Level.FINER, "Extracting files...");
+	public static boolean filesEqual(File solutionOutputFile, File testOutputFile) throws IOException {
+		try (Scanner solutionOutput = new Scanner(new BufferedInputStream(new FileInputStream(solutionOutputFile)));
+				Scanner testOutput = new Scanner(new BufferedInputStream(new FileInputStream(testOutputFile)));) {
+			while (solutionOutput.hasNextLine() && testOutput.hasNextLine()) {
+				if (!solutionOutput.nextLine().equals(testOutput.nextLine())) {
+					return false;
+				}
+			}
+			return solutionOutput.hasNextLine() == testOutput.hasNextLine();
+		}
+	}
 
-        ZipFile zip = new ZipFile(getZipFile());
-        Enumeration<? extends ZipEntry> zEntries = zip.entries();
+	public static void extractFilesHelper(File unzipDir) throws IOException {
+		// If the directory already existed, we may have to clean up old files
+		if (!unzipDir.mkdir()) {
+			cleanup(unzipDir);
+		}
 
-        while (zEntries.hasMoreElements()) {
-            ZipEntry curr = zEntries.nextElement();
-            Files.copy(zip.getInputStream(curr),
-                    new File(unzipDir.getCanonicalPath() + "/" + curr.getName()).toPath());
-        }
-        logger.log(Level.FINEST, String.format("Finished extracting %d files", unzipDir.listFiles().length));
-    }
+		logger.log(Level.FINER, "Extracting files...");
 
-    public static void cleanup(File dir) {
-        Arrays.stream(dir.listFiles())
-                .filter(f -> !f.getName().endsWith(".zip"))
-                .forEach(f -> {
-                    if (f.isDirectory()) cleanup(f);
-                    f.delete();
-                });
-    }
+		ZipFile zip = new ZipFile(getZipFile());
+		Enumeration<? extends ZipEntry> zEntries = zip.entries();
 
-    public static File getZipFile() {
-        return Arrays.stream(DATA_DIR.listFiles())
-                .filter(f -> f.getName().endsWith(".zip"))
-                .filter(f -> f.getName().contains(PROBLEM))
-                .findAny()
-                .orElseThrow(() -> new IllegalArgumentException(
-                        String.format("Missing data for %s.", PROBLEM)));
-    }
+		while (zEntries.hasMoreElements()) {
+			ZipEntry curr = zEntries.nextElement();
+			Files.copy(zip.getInputStream(curr), new File(unzipDir.getCanonicalPath() + "/" + curr.getName()).toPath());
+		}
+		logger.log(Level.FINEST, String.format("Finished extracting %d files", unzipDir.listFiles().length));
+	}
+
+	public static void cleanup(File dir) {
+		Arrays.stream(dir.listFiles()).filter(f -> !f.getName().endsWith(".zip")).forEach(f -> {
+			if (f.isDirectory()) cleanup(f);
+			f.delete();
+		});
+	}
+
+	public static File getZipFile() {
+		return Arrays.stream(DATA_DIR.listFiles()).filter(f -> f.getName().endsWith(".zip"))
+				.filter(f -> f.getName().contains(PROBLEM)).findAny()
+				.orElseThrow(() -> new IllegalArgumentException(String.format("Missing data for %s.", PROBLEM)));
+	}
 }
