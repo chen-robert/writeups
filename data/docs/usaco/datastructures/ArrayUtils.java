4c06ef78d29b6aa182ab5f96e871148f667e8606
Robert Chen <robertchen@live.com>
2019-08-10 08:50:55 +0800

    Updated ArrayUtils

--- data/docs/usaco/datastructures/ArrayUtils.java
+++ data/docs/usaco/datastructures/ArrayUtils.java
@@ -2,14 +2,9 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class ArrayUtils {
-  static final Random rand = new Random(System.nanoTime());
+  static Random rand = new Random(System.nanoTime());
 
-  public static void sort(int[] a) {
-    shuffle(a);
-    Arrays.sort(a);
-  }
-
-  public static void shuffle(int[] a) {
+  static void shuffle(int[] a) {
     for (int i = 0; i < a.length; i++) {
       int j = rand.nextInt(i + 1);
 
